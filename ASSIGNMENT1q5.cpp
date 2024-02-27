cc676a51b410d837ccd6458c660faca53f345a1a
sanskarchourasia <160160600+sanskarchourasia@users.noreply.github.com>
2024-02-27 23:23:21 +0530

    Add files via upload

---
+++ ASSIGNMENT1q5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter radius of the circle : "<<endl;
+    cin>>r;
+    float c;
+    c=2*3.14*r;
+    cout<<"The circumference of the circle is : "<<c;
+
+}
