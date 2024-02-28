33a70db2fd9077f6bada1f7d40922154c80ad524
sanskarchourasia <160160600+sanskarchourasia@users.noreply.github.com>
2024-02-28 22:20:07 +0530

    Add files via upload

---
+++ ASSIGNMENT2q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter the radius of cylender : ";
+    cin>>r;
+    float h;
+    cout<<"Enter the hieght of cylender : ";
+    cin>>h;
+    float c;
+    c=3.14*r*r*h;
+    cout<<"The required circumference of the circle is : "<<c;
+
+}
